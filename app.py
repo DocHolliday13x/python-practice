e2ca2de0a1018c6ee5bbf17728829c05f5da8551
Ryan Eastman <rking1323@gmail.com>
2023-09-22 13:26:08 -0700

    tested each route w/POSTMAN + mock data, routes successful

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+#global mock data variable
+mock_data = {
+        "user_id": 1,
+        "name": "John Doe",
+        "age": 30,
+        "email": "john.doe@example.com"
+    }
+
+
 @app.route('/')
 def home():
     return "Home Page"
@@ -10,17 +19,26 @@ def home():
 def get():
     return "Get Request"
 
-#mock data for testing
+# #mock data for testing
+# @app.route('/get_mock', methods=['GET'])
+# def get_mock():
+#     #create a dictionary
+#     mock_data = {
+#         "user_id": 1,
+#         "name": "John Doe",
+#         "age": 30,
+#         "email": "john.doe@example.com"
+#     }
+    
+#     extra = request.args.get('extra')
+#     if extra:
+#         mock_data['extra'] = extra
+      
+#     #jsonify the dictionary and pass status code 
+#     return jsonify(mock_data), 200
+
 @app.route('/get_mock', methods=['GET'])
 def get_mock():
-    #create a dictionary
-    mock_data = {
-        "user_id": 1,
-        "name": "John Doe",
-        "age": 30,
-        "email": "john.doe@example.com"
-    }
-    
     extra = request.args.get('extra')
     if extra:
         mock_data['extra'] = extra
@@ -44,10 +62,33 @@ def create_user():
 def put():
     return "Put Request"
 
+@app.route('/put/<int:user_id>', methods=['PUT'])
+def update(user_id):
+    data = request.get_json()
+    
+    if 'user_id' in mock_data and mock_data['user_id'] == user_id:
+        mock_data['name'] = data.get('name', mock_data['name'])
+        mock_data['age'] = data.get('age', mock_data['age'])
+        mock_data['email'] = data.get('email', mock_data['email'])
+        
+        return jsonify(mock_data), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+    
 
 @app.route('/delete', methods=['DELETE'])
 def delete():
     return "Delete Request"
+
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    # Check if the user_id exists in the mock_data dictionary
+    if 'user_id' in mock_data and mock_data['user_id'] == user_id:
+        # Remove the user from the mock_data dictionary
+        del mock_data['user_id']
+        return "User deleted successfully", 200
+    else:
+        return "User not found", 404
 
 
 if __name__ == '__main__':
