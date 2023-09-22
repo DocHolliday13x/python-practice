c71ec2262f04d5189070729c915e0dd3fc999e75
Ryan Eastman <rking1323@gmail.com>
2023-09-22 12:11:14 -0700

    tested POST route w/POSTMAN, 201 status

--- app.py
+++ app.py
@@ -28,13 +28,22 @@ def get_mock():
     #jsonify the dictionary and pass status code 
     return jsonify(mock_data), 200
 
+
 @app.route('/post', methods=['POST'])
 def post():
     return "Post Request"
 
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    
+    return jsonify(data), 201
+
+
 @app.route('/put', methods=['PUT'])
 def put():
     return "Put Request"
+
 
 @app.route('/delete', methods=['DELETE'])
 def delete():
