8f042d515157175490c9b46c2d2e8cb834a6f727
Ryan Eastman <rking1323@gmail.com>
2023-09-22 08:19:30 -0700

    created basic routes

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def home():
     return "Home Page"
 
+@app.route('/get', methods=['GET'])
+def get():
+    return "Get Request"
+
+@app.route('/post', methods=['POST'])
+def post():
+    return "Post Request"
+
+@app.route('/put', methods=['PUT'])
+def put():
+    return "Put Request"
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    return "Delete Request"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
